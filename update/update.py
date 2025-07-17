dafb1f7b3c3b4a3ddcaa3110a0c2b9f520862b7b
remin-p <remin-p@proton.me>
2025-07-17 11:23:51 -0300

    style: moved print for future reference, improved consistency, added new tasks

--- update/update.py
+++ update/update.py
@@ -14,7 +14,7 @@ with open('update/last_commit.txt','r') as file:
 
 diffs = repo.commit(last_commit).diff(repo.head.commit) # Compare the _oldest_ commit to HEAD otherwise it's not going to detect which files have been deleted.
 
-print("Last commit being compared:", last_commit)
+print('Last commit being compared:', last_commit)
 print(f"Number of diffs found: {len(diffs)}")
 
 for d in diffs:
@@ -23,8 +23,8 @@ for d in diffs:
     elif d.a_path and d.b_path: # Check if both paths exist
         sendFiles.append(d.a_path)
     
-print("Files to send:", sendFiles)
-print("Files to delete:", deleteFiles)
+print('Files to send:', sendFiles)
+print('Files to delete:', deleteFiles)
 
 print(repo.head.reference.log()[-1][1]) # Grabs the latest commit hash
 
@@ -32,6 +32,7 @@ sendFiles = [item for item in sendFiles if not any(str(item).startswith(target) 
 deleteFiles = [item for item in deleteFiles if not any(str(item).startswith(target) for target in noSend)]
 
 # Make a request to upload these files
+# TODO: Make request to delete files too
 
 NEKOWEB_API_KEY = os.getenv('NEKOWEB_API_KEY')
 
@@ -50,3 +51,7 @@ for file in sendFiles:
         raise SystemExit(err)
 
     print(file, response.text)
+
+# TODO: Update the last_commit.txt with the latest commit hash
+
+print(repo.head.reference.log()[-1][1]) # Grabs the latest commit hash
