784de1e76787025988b3222094ffb170ac0862aa
remin-p <remin-p@proton.me>
2025-07-17 11:17:14 -0300

    feat: separates which files to send and to delete

--- update/update.py
+++ update/update.py
@@ -6,20 +6,30 @@ import os
 
 repo = Repo('.')
 sendFiles = []
+deleteFiles = []
 noSend = ['.github/','update/']
 
 with open('update/last_commit.txt','r') as file: 
     last_commit = file.read().strip()
 
-diffs = repo.head.commit.diff(last_commit)
+diffs = repo.commit(last_commit).diff(repo.head.commit) # Compare the _oldest_ commit to HEAD otherwise it's not going to detect which files have been deleted.
+
+print("Last commit being compared:", last_commit)
+print(f"Number of diffs found: {len(diffs)}")
 
 for d in diffs:
-    sendFiles.append(d.a_path)
+    if d.deleted_file:
+        deleteFiles.append(d.a_path)
+    elif d.a_path and d.b_path: # Check if both paths exist
+        sendFiles.append(d.a_path)
+    
+print("Files to send:", sendFiles)
+print("Files to delete:", deleteFiles)
 
 print(repo.head.reference.log()[-1][1]) # Grabs the latest commit hash
 
 sendFiles = [item for item in sendFiles if not any(str(item).startswith(target) for target in noSend)]
-print(sendFiles)
+deleteFiles = [item for item in deleteFiles if not any(str(item).startswith(target) for target in noSend)]
 
 # Make a request to upload these files
 
