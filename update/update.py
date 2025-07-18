1ddb83427db1a0894df505e5bddc6b548251eacc
remin-p <remin-p@proton.me>
2025-07-18 08:46:59 -0300

    style: lint

--- update/update.py
+++ update/update.py
@@ -8,12 +8,14 @@ import sys
 repo = Repo('.')
 sendFiles = []
 deleteFiles = []
-noSend = ['.github/','update/']
+noSend = ['.github/', 'update/']
 
-with open('update/last_commit.txt','r') as file: 
+with open('update/last_commit.txt', 'r') as file:
     last_commit = file.read().strip()
 
-diffs = repo.commit(last_commit).diff(repo.head.commit) # Compare the _oldest_ commit to HEAD otherwise it's not going to detect which files have been deleted.
+# We must first compare the _oldest_ commit to HEAD otherwise it's not going to
+# detect which files have been deleted.
+diffs = repo.commit(last_commit).diff(repo.head.commit)
 
 print('Last commit being compared:', last_commit)
 print(f"Number of diffs found: {len(diffs)}")
@@ -21,7 +23,7 @@ print(f"Number of diffs found: {len(diffs)}")
 for d in diffs:
     if d.deleted_file:
         deleteFiles.append(d.a_path)
-    elif d.a_path and d.b_path: # Check if both paths exist
+    elif d.a_path and d.b_path:  # Check if both paths exist
         sendFiles.append(d.a_path)
 
 sendFiles = [item for item in sendFiles if not any(str(item).startswith(target) for target in noSend)]
@@ -35,11 +37,11 @@ print('=> Files to delete:', deleteFiles)
 NEKOWEB_API_KEY = os.getenv('NEKOWEB_API_KEY')
 
 url = 'https://nekoweb.org/api/files/upload'
-headers = { 'Authorization': NEKOWEB_API_KEY }
+headers = {'Authorization': NEKOWEB_API_KEY}
 
 for file in sendFiles:
-    files = { 'files': (file, open(file, 'rb'), 'application/octet-stream') }
-    data = { 'pathname': '.' if file.find('/') == -1 else file[:file.rfind('/')+1] }
+    files = {'files': (file, open(file, 'rb'), 'application/octet-stream')}
+    data = {'pathname': '.' if file.find('/') == -1 else file[:file.rfind('/')+1]}
 
     try:
         response = requests.request('POST', url, headers=headers, data=data, files=files)
@@ -57,7 +59,7 @@ if len(deleteFiles) <= 0:
     sys.exit()
 
 url = 'https://nekoweb.org/api/files/delete'
-headers = { 'Authorization': NEKOWEB_API_KEY, 'content-type':'application/x-www-form-urlencoded' }
+headers = {'Authorization': NEKOWEB_API_KEY, 'content-type':'application/x-www-form-urlencoded'}
 
 for file in deleteFiles:
     payload = f"pathname={file}"
@@ -65,15 +67,15 @@ for file in deleteFiles:
     try:
         response = requests.request('POST', url, headers=headers, data=payload)
         response.raise_for_status()
- 
+
     except requests.exceptions.HTTPError as err:
         if response.status_code == 400:
             print(f"=X 400, Failed to upload file {file} (payload '{payload}'): {response.text}!")
             break
         raise SystemExit(err)
 
-    print(f"-> File '{file}' delete (Payload '{payload}'), response: {response.status_code,response.text} <-")
+    print(f"-> File '{file}' delete (Payload '{payload}'), response: {response.status_code, response.text} <-")
 
 # TODO: Update the last_commit.txt with the latest commit hash
 
-print(repo.head.reference.log()[-1][1]) # Grabs the latest commit hash
+print(repo.head.reference.log()[-1][1])  # Grabs the latest commit hash
